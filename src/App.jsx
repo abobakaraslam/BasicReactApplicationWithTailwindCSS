f920a4cae8b8b637bff8f11b4576bdc394ac330c
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2024-05-28 12:51:49 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>Web Development Course</h1>
+        <p>
+          In this course, you will learn about React, Next, Nest, Express and
+          mongoDB
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
