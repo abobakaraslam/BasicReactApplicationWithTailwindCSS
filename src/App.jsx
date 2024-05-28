5e57e802de8cacce819e2a81ecc975f0902dfabd
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2024-05-28 15:38:14 +0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <div className="text-center">
+      <div className="text-center m-6">
         <h1>Web Development Course</h1>
         <p>
           In this course, you will learn about React, Next, Nest, Express and
